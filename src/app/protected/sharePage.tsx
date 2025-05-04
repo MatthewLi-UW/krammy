051e0e2c9840773575a8d90d3557e76e253bae0b
Matthew Li <matthewlischoolemail@gmail.com>
2025-05-04 01:16:14 -0400

    fix expiry parameter error

--- src/app/protected/sharePage.tsx
+++ src/app/protected/sharePage.tsx
@@ -15,7 +15,7 @@ const RecieveDeckForm = () => {
     e.preventDefault();
     setError(''); // Reset any previous errors
     try {
-      const { shareLink, expiresAt } = await shareADeck(parseInt(deckID), accessType);
+      const { shareLink, expiresAt } = await shareADeck(parseInt(deckID), accessType, 10000);
       console.log(deckID)
       console.log(accessType)
       setShareLink(shareLink);

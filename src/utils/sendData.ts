8362ddd109d92572b3cd640cadadf502d3090e68
Kevin C <kc654723@gmail.com>
2025-05-04 22:16:42 -0400

    working

--- src/utils/sendData.ts
+++ src/utils/sendData.ts
@@ -67,3 +67,24 @@ export const shareADeck = async (deckID: number, access_type: AccessType = ACCES
     throw error; 
   }
 };
+
+export const sentStats = async (userid: string, accuracy: number, wpm: number,deckID?: number, cardID?: number) => {
+  console.log("sentStats")
+  if (deckID){
+    const { data: deckStats, error: deckError } = await supabase
+    .from('DeckMetrics')
+    .insert({user_id: userid, deck_id: deckID, accuracy: accuracy, wpm: wpm}).select();
+    if (deckError) throw deckError;
+    console.log(deckStats)
+    return deckStats;
+  } else if (cardID) {
+    const { data: deckStats, error: deckError } = await supabase
+    .from('DeckMetrics')
+    .insert({user_id: userid, card_id: cardID, accuracy: accuracy, wpm: wpm}).select();
+    if (deckError) throw deckError;
+    console.log(deckStats)
+    return deckStats;
+  } else {
+    console.log("ERROR not a valid sentStats")
+  }
+}

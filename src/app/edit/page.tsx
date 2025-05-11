3ca7cdec456fe89abd9c3c3bd8a65908006e6f6c
Matthew Li <matthewlischoolemail@gmail.com>
2025-05-11 13:51:47 -0400

    comment out old edit button

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -617,7 +617,8 @@ const checkCardPositions = async () => {
               <p className="text-lg text-[var(--color-text-dark)]">{card.back}</p>
             </div>
             <div className="absolute top-4 right-4 flex space-x-2">
-              <button
+              {/* Old edit button */}
+              {/* <button
                 onClick={(e) => {
                   e.stopPropagation();
                   startEditing();
@@ -628,7 +629,7 @@ const checkCardPositions = async () => {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                 </svg>
-              </button>
+              </button> */}
               <button
                 onClick={(e) => {
                   e.stopPropagation();
